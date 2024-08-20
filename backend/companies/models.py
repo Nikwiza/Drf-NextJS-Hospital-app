8e83744fc45c43213698b55b38ebeb2deb461384
dusaan21 <dusan.a.stojanovic@gmail.com>
2024-08-20 18:04:58 +0200

    Change company model

--- backend/companies/models.py
+++ backend/companies/models.py
@@ -1,5 +1,17 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-class Companie(models.Model):
-    companie_name = models.CharField(max_length=80, unique=True)
-    description = models.CharField(max_length=255,unique=True)
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    description = models.TextField()
+    working_hours = models.CharField(max_length = 15, default = 'unspecified')
+    average_rating = models.FloatField(
+        default=0.00,
+        validators=[MinValueValidator(0.00), MaxValueValidator(5.00)]
+    )
+    #equipment = models.ManyToManyField(Equipment, related_name='equipment_list', blank=True)
+
+    def __str__(self):
+        return self.name
