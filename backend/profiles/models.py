3c4221cbd1d46d5ef405c4c2f116b2381dccd594
dusaan21 <dusan.a.stojanovic@gmail.com>
2024-08-20 20:22:55 +0200

    Create appointments model

--- backend/profiles/models.py
+++ backend/profiles/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from user.models import Account
 from companies.models import Company
 
-class CompanieAdministrator(models.Model):
+class CompanyAdministrator(models.Model):
     account = models.OneToOneField(Account, on_delete=models.CASCADE)
     company = models.OneToOneField(Company, on_delete=models.DO_NOTHING)
 
