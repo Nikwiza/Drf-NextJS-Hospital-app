c17c7750f5b0ede0885d90590beb2b68f1ea9bd3
Nikwiza <nikola.kerleta1@gmail.com>
2024-08-26 18:40:13 +0200

    Initial authentication system

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,1 +1,12 @@
-//Landing page
+import React from "react";
+
+const HomePage = async () => {
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-5xl max-[500px]:text-2xl">HomePage</h1>
+    </div>
+  );
+};
+
+export default HomePage;
